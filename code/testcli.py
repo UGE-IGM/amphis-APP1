11d0d305de2e72db94820f41fc0cb8896381d6ee
Antoine Meyer <antoine.meyer@univ-eiffel.fr>
2024-09-24 09:19:54 +0200

    Après amphi 3

---
+++ code/testcli.py
@@ -0,0 +1,14 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog='ProgramName',
+    description='What the program does',
+    epilog='Text at the bottom of help')
+
+parser.add_argument('filename')           # positional argument
+parser.add_argument('-c', '--count')      # option that takes a value
+parser.add_argument('-v', '--verbose',
+                    action='store_true')  # on/off flag
+
+args = parser.parse_args()
+print(args.filename, args.count, args.verbose)
